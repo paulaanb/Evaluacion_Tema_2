614236feddf008d85f12a7e5cdfceda76a47f162
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:36:07 +0200

    modificamos ejercico 4-3

--- Ejercicio4/ejercicio4-3.py
+++ Ejercicio4/ejercicio4-3.py
@@ -19,3 +19,18 @@ Traceback (most recent call last):
   File "<ejercicio4-3>", line 5, in <module>
     error_index(alemania)
 NameError: name 'alemania' is not defined
+
+#Otra forma de error
+try:
+   paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+   paises["alemania"]
+except KeyError:
+    print("ERROR: acceso a clave incorrecta en diccionario")
+else:
+    print("Manejo de diccionario correcto")
+finally:
+    print("Fin del bloque try")
+
+
+ERROR: acceso a clave incorrecta en diccionario
+Fin del bloque try
