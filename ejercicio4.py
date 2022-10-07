8394eff14097c030cb1b3cae570d03246f2309fb
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:13:58 +0200

    Realizamos el codigo 1 con error de ZeroDivisionError

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+#Identificar los errores en los siguiente bloques de código y evaluarlos con excepciones especificas para evitar errores no controlados en nuestros programas. Añade m mensajes explicativos para el usuario.
+#1) Código a evaluar:
+
+numero = 7/0
+
+def division(a, b):
+   try:
+       result = a / b
+     except ZeroDivisionError:
+      print('¡No se puede dividir por cero!')
+    else:
+       print(result)
