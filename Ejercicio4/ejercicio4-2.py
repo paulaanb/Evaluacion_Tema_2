6670990b00cb2efaef06899a538cefe90e0f8768
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:23:36 +0200

    Tipo de error y como corregirlo

---
+++ Ejercicio4/ejercicio4-2.py
@@ -0,0 +1,10 @@
+#2) Código a evaluar:
+
+lista = [4, 7, 30, 23, 5]
+
+lista[10]
+lista[5:5] = [10]
+Traceback (most recent call last):
+  File "<lista>", line 5, in <module>
+    lista[10]
+  Index error: list index out of range
