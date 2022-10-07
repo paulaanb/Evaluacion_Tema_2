f3a3f78bf45caaf2afcba13b15fcb3f3555b6872
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:35:01 +0200

    modificamos ejercicio 4-2

--- Ejercicio4/ejercicio4-2.py
+++ Ejercicio4/ejercicio4-2.py
@@ -8,3 +8,14 @@ Traceback (most recent call last):
   File "<lista>", line 5, in <module>
     lista[10]
   Index error: list index out of range
+
+#Otra forma de error
+except IndexError:
+    print("ERROR: acceso a índice incorrecto en lista")
+else:
+    print("Manejo de listas correcto")
+finally:
+    print("Fin del bloque try")
+
+ERROR: acceso a índice incorrecto en lista
+Fin del bloque try
