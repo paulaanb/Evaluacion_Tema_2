7d929d8ab89d0e85ac86b5279829dad393c505b3
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:51:14 +0200

    clase bicicleta modificada

--- Ejercicio5/bicicleta.py
+++ Ejercicio5/bicicleta.py
@@ -1,3 +1,6 @@
-class Bicicleta():
-    pass
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo   
+     pass
 
