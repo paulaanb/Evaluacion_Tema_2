017c44e5c0c2ef9a7d8c31c6d9fd9aff13c58eb4
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:08:02 +0200

    Creamos el constructor de la clase con print para comprobar que el producto se haya creado con exito

--- ejercicio3.py
+++ ejercicio3.py
@@ -9,3 +9,56 @@ class Producto:
  
     def __str__(self):
         return """\
+
+codigo\t{}
+nombre\t\t{}
+precio\t\t{}
+tipo\t{}""".format(self.codigo, self.nombre, self.precio, self.tipo)
+ 
+ 
+@property
+ def codigo(self):
+ 
+    return self.__codigo
+ 
+@codigo.setter
+ def codigo(self, nuevoValor):
+ 
+    self.__codigo = nuevoValor
+ 
+@property
+ def nombre(self):
+ 
+    return self.__nombre
+ 
+@nombre.setter
+ def nombre(self, nuevoValor):
+ 
+    self.__nombre = nuevoValor
+ 
+@property
+ def precio(self):
+ 
+    return self.__precio
+ 
+@precio.setter
+ def precio(self, nuevoValor):
+ 
+    self.__precio = nuevoValor
+ 
+@property
+ def tipo(self):
+ 
+    return self.__tipo
+ 
+@tipo.setter
+ def tipo(self, nuevoValor):
+ 
+    self.__tipo = nuevoValor
+
+#Creamos el constructor de la clase con print para comprobar que el producto se haya creado con exito
+p1= (12, "leche", 1, "lacteo")
+p2= (10, "pollo", 4, "carne")
+print("\nProbando el producto con un print:")
+print(p1)
+print(p2)
