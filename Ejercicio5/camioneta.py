a73c92c83626738264c05decbf24d824d10385ea
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:53:00 +0200

    clase camioneta modificada

--- Ejercicio5/camioneta.py
+++ Ejercicio5/camioneta.py
@@ -1,4 +1,7 @@
-class Camioneta():
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga
     pass
 
 
