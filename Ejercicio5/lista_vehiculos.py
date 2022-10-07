d2d54aec7ed0230e9e60ef3c9753e553ebe67009
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:57:21 +0200

    completamos la lista de vehiculos y creamos uno que corresponfa a cada clase

---
+++ Ejercicio5/lista_vehiculos.py
@@ -0,0 +1,10 @@
+from Ejercicio5 import *
+lista_vehiculos = [a, b, c, d, e]
+#Creamos un vehiculo por cada clase creada
+a = Camioneta("rojo", 8, 120, 400, 500)
+b = Coche("azul", 4, 120, 800)
+c = Bicicleta("amarilla", 2, "triciclo")
+d = Coche("verde", 4, 150, 800)
+e = Motocicleta("negra", 2, "triciclo", 150, 200)
+lista_vehiculos = [a, b, c, d, e]
+catalogar(lista_vehiculos)
