07ad8996bb63f15d65c5a619c8e7bc454301f0c3
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 17:21:03 +0200

    Creamos un test rapido porque no hay tiempo de hacer uno mas largo

--- test/test_database.py
+++ test/test_database.py
@@ -4,3 +4,10 @@ import helpers
 import config
 import csv
 import database as db
+
+class Pruebas(unittest.TestCase):
+    def test(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
