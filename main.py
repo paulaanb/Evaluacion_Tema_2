0453c1c10a2b90cd2bbff10733b0c1995dfce4e7
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 17:11:38 +0200

    Creamos el archivo main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from ejercicio1 import *
+from ejercicio2 import *
+from ejercicio3 import *
+from Ejercicio4 import *
+from Ejercicio5 import *
+
+
+if __name__ == "__main__":
+  print("El ejercicio se va a ejecutar a continuacion.")
+else:
+   print("El ejercicio se ejecutara cuando se haya exportado previamente.")
+
+1= ejercicio1
+2= ejercicio2
+3= ejercicio3
+4= Ejercicio4
+5= Ejercicio5
+print("Seleccione el ejercicio que desea ejecutar (1-5):")
+if ejercicio_elegido = 1:
+    print ("Ejercicio 1 \n")
+elif ejercicio_elegido = 2:
+    print ("Ejercicio 2 \n")
+elif ejercicio_elegido = 3:
+    print ("Ejercicio 3 \n")
+elif ejercicio_elegido = 4:
+    print ("Ejercicio 4 \n")
+else:
+    print ("Ejercicio 5 \n")
