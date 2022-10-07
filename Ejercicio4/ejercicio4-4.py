450da9c07362a31db0bb0027f01a3133d66527d0
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-07 16:37:02 +0200

    Terminamos el ejercicio 4

--- Ejercicio4/ejercicio4-4.py
+++ Ejercicio4/ejercicio4-4.py
@@ -1,3 +1,15 @@
 #4) Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:
 
 resultado = "2" + 10
+
+try:
+    resultado = "2" + 10
+except TypeError:
+    print("ERROR: intento de suma de número y cadena")
+else:
+    print("Operación realizada correctamente")
+finally:
+    print("Fin del bloque try")
+
+ERROR: intento de suma de número y cadena
+Fin del bloque try
